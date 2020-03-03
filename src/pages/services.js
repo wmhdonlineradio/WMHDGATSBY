1dfe415de1dcf51a6d042a39380708a1de12112e
Keith C-R <keith@kmddigital.com>
2020-03-03 16:54:43 -0500

    Updated services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -15,7 +15,7 @@ const ServicesPage = ({data}) => (
             <Img fluid={data.djServices.childImageSharp.fluid} className="services-pic" />
             <p>WMHD offers DJ services for on-campus events. If you would like us to DJ at your event, 
             please fill out our rental request and DJing services <a href="https://docs.google.com/forms/d/e/1FAIpQLSeggZD4Busmn-YDP0NbEWqNMB_leOIePlblqkMUsb9WULST9g/viewform">form</a>. 
-            If DJ services are required, then we will set up the necessary equipment that we own and then play the music requested for the event.</p>
+            If DJ services are required, then WMHD Personnel will set up the equipment and play the requested music.</p>
           </div>
           <div className="col-md-4">
             <h3>Equipment Rentals</h3>
@@ -35,12 +35,12 @@ const ServicesPage = ({data}) => (
           <div className="col-md-4">
             <h3>Guitar Lessons</h3>
             <Img fluid={data.guitarLessons.childImageSharp.fluid} className="services-pic" />
-            <p>Learn the basics of guitar playing every Saturday from 3:00pm to 5:00pm. These free lessons will get you started on electric or acoustic guitar.</p>
+            <p>Learn the basics of guitar playing every Saturday from 3:00pm to 5:00pm. These free lessons will get you started on electric or acoustic guitar. Contact Laya Mehra for more details.</p>
           </div>
           <div className="col-md-4">
             <h3>Production Lessons</h3>
             <Img fluid={data.productionLessons.childImageSharp.fluid} className="services-pic" />
-            <p>Learn to produce your own music every Saturday from 2:00pm to 3:00pm. You'll have access to our expansive program collection and equipment.</p>
+            <p>Learn to produce your own music every Saturday from 2:00pm to 3:00pm. You'll have access to our expansive software and equipment. Contact Frank Lancaster for more details.</p>
           </div>
           <div className="col-md-4">
             <h3>Podcast Studio</h3>
@@ -48,6 +48,7 @@ const ServicesPage = ({data}) => (
             <p>Our Podcast Studio is free to book for recording any voice-based audio. There are high quality vocal microphones for 3 different speakers and more can be added if needed.</p>
           </div>
         </div>
+        <p className="services-info">For any questions regarding our services or to book one of our studios, contact our <a href="mailto:wmhdradio@gmail.com">officers</a>.</p>
     </section>
   </>
 )
