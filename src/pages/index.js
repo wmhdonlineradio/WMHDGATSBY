89332927048c7b0971fc08403988c50b900237a1
ASingularPhoton <68510231+ASingularPhoton@users.noreply.github.com>
2024-10-22 15:10:18 -0400

    hi

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const links = [
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "AMGOUS",
   },
   {
     text: "Examples",
