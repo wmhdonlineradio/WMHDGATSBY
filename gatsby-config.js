1d417fc6ff9f0af568cabdf89e20a720b45228c5
ASingularPhoton <68510231+ASingularPhoton@users.noreply.github.com>
2024-11-05 19:08:02 -0500

    fix header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `WMHD Radio`,
+    description: `WMHD Radio is the campus radio station of Rose-Hulman Institute of Technology. 24/7 ad-free music.`,
+    author: `@wmhdonlineradio`,
   },
   plugins: [
-    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,16 +18,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `WMHD Online Radio`,
+        short_name: `WMHD Radio`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#800000`,
+        theme_color: `#800000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
+    `gatsby-plugin-smoothscroll`,
   ],
 }
