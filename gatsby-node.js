7db100bbe9dec1dcfb41ebd312818d9fea4e6ff4
ASingularPhoton <68510231+ASingularPhoton@users.noreply.github.com>
2025-01-24 20:28:42 -0500

    discord forwarding

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,21 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions;
+
+  // Create a deferred static generation (DSG) page
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
-  })
-}
+  });
+
+  // Add a redirect for /discord to a Discord invite link
+  createRedirect({
+    fromPath: "/discord",
+    toPath: "https://discord.gg/YOUR_DISCORD_INVITE_LINK",
+    isPermanent: true,
+    redirectInBrowser: true,
+  });
+};
---
+++ src/Templates/using-dsg.txt
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UsingDSG = () => {
+  return (
+    <main>
+      <h1>Deferred Static Generation (DSG) Example</h1>
+      <p>This page was built using Gatsby's DSG feature!</p>
+    </main>
+  );
+};
+
+export default UsingDSG;
